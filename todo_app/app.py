4cf2b9e12c2aceb07a6c2f1f2dbce5d46c9460c7
Chinedu Oty <otychinedu@gmail.com>
2021-04-28 13:14:29 +0100

    Made changes from review

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 import json
 from todo_app.flask_config import Config
 from todo_app.data import session_items as session
@@ -7,30 +7,34 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["GET"])
 def index():
-    error_message = None
-    if request.method == "POST":
-        title = request.form.get('title', '').strip()
-        if title == '':
-            error_message = 'Please enter a task'
-        else:
-            session.add_item(title)        
-
     sort_status = request.args.get('sort_by_status', 'DESC')
     items = sorted(session.get_items(), key=lambda x: x['status'], reverse=False if sort_status == 'ASC' else True)
-    return render_template('index.html', todos=items, error=error_message)
+    return render_template('index.html', todos=items)
+
+@app.route('/', methods=["POST"])
+def save_item():
+    title = request.form.get('title', '').strip()
+    if title == '':
+        flash('Please enter a task')
+    else:
+        session.add_item(title)        
+
+    return redirect(url_for('index'))
 
 @app.route('/mark-completed', methods=["POST"])
 def mark_completed():
-    item = json.loads(request.form['todo_item'])
+    todo_id = request.form['todo_id']
+    item = session.get_item(todo_id)
     item['status'] = 'Completed'
     session.save_item(item)
     return redirect(url_for('index'))
 
 @app.route('/delete-item', methods=["POST"])
 def delete_item():
-    item = json.loads(request.form['todo_item'])
+    todo_id = request.form['todo_id']
+    item = session.get_item(todo_id)
     session.remove_item(item)
     return redirect(url_for('index'))
 
