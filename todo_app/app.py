e70ce07f1ab5bf1bad1d9db5072e36d1b6ef0106
Chinedu Oty <otychinedu@gmail.com>
2021-06-04 21:59:40 +0100

    Completed Module 2

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,42 +1,64 @@
+from datetime import date
 from flask import Flask, request, render_template, redirect, url_for, flash
-import json
 from todo_app.flask_config import Config
-from todo_app.data import session_items as session
+from todo_app.service import ItemService
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+service = ItemService(app)
 
 @app.route('/', methods=["GET"])
 def index():
     sort_status = request.args.get('sort_by_status', 'DESC')
-    items = sorted(session.get_items(), key=lambda x: x['status'], reverse=False if sort_status == 'ASC' else True)
+    items = sorted(service.get_all_items(), key=lambda x: x.status,
+                   reverse=False if sort_status == 'ASC' else True)
     return render_template('index.html', todos=items)
+
 
 @app.route('/', methods=["POST"])
 def save_item():
     title = request.form.get('title', '').strip()
-    if title == '':
-        flash('Please enter a task')
-    else:
-        session.add_item(title)        
+    description = request.form.get('description', '').strip()
+    due_date = request.form.get('due-date', '').strip()
+
+    if validate_input(title, due_date):
+        service.create_item(title, description, due_date)
 
     return redirect(url_for('index'))
 
-@app.route('/mark-completed', methods=["POST"])
-def mark_completed():
+
+@app.route('/complete-item', methods=["POST"])
+def complete_item():
     todo_id = request.form['todo_id']
-    item = session.get_item(todo_id)
-    item['status'] = 'Completed'
-    session.save_item(item)
+    service.complete_item(todo_id)
     return redirect(url_for('index'))
+
+
+@app.route('/repeat-item', methods=["POST"])
+def repeat_item():
+    todo_id = request.form['todo_id']
+    service.repeat_item(todo_id)
+    return redirect(url_for('index'))
+
 
 @app.route('/delete-item', methods=["POST"])
 def delete_item():
     todo_id = request.form['todo_id']
-    item = session.get_item(todo_id)
-    session.remove_item(item)
+    service.delete_item(todo_id)
     return redirect(url_for('index'))
+
+
+def validate_input(title_input, date_input):
+    if title_input == '':
+        flash('Please enter a task')
+        return False
+    if date_input:
+        chosen_date = date.fromisoformat(date_input)
+        if date.today() > chosen_date:
+            flash('Please enter a valid date in the future')
+            return False
+    return True
 
 
 if __name__ == '__main__':
--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -4,5 +4,16 @@ import os
 class Config:
     """Base configuration variables."""
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    TRELLO_API_KEY = os.environ.get('TRELLO_API_KEY')
+    TRELLO_TOKEN = os.environ.get('TRELLO_TOKEN')
+    TRELLO_BOARD_NAME = 'Tasky_1_2_3_'
+
     if not SECRET_KEY:
-        raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
+        raise ValueError(
+            "No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
+    if not TRELLO_API_KEY:
+        raise ValueError(
+            "No TRELLO_API_KEY set for Flask application. Did you follow the setup instructions?")
+    if not TRELLO_TOKEN:
+        raise ValueError(
+            "No TRELLO_TOKEN set for Flask application. Did you follow the setup instructions?")
